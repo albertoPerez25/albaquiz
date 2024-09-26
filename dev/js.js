fbf927424d641d50ee64ee7a4540511d16ce009c
alberto <alberto.perez25@alu.uclm.es>
2024-09-26 17:57:32 +0200

    si esto no va ya nose que hacer

--- dev/js.js
+++ dev/js.js
@@ -1,11 +1,6 @@
 async function setMotionListeners(){
-    // ORIENTATION LISTENER
-    /*await window.addEventListener('orientation', event => {
-        console.log('Device orientation event: %O', event)
-    })*/
-
     await window.addEventListener('devicemotion', event => {
-        if ((event.rotationRate.alpha > 256 || event.rotationRate.beta > 256 || event.rotationRate.gamma > 256)) {
+        if ((event.rotationRate.alpha > 400 || event.rotationRate.beta > 400 || event.rotationRate.gamma > 400)) {
             this.output_message.innerHTML = "SE AGITAAAAAAAAA!"
             document.body.style.backgroundColor = "red"
             resetear()
@@ -19,5 +14,5 @@ function resetear(){
         document.body.style.backgroundColor = "grey"
     }, 2000)
 }
-//resetear()
+
 this.setMotionListeners()
