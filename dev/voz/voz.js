0df777d027a6b4c1aca6eb0099173866837ced3b
alberto <alberto.perez25@alu.uclm.es>
2024-10-06 19:24:36 +0200

    Quitado el +

--- dev/voz/voz.js
+++ dev/voz/voz.js
@@ -12,7 +12,7 @@ let p = document.createElement("p");
 recognition.addEventListener("result", (e) => {
   console.log(e);
   document.getElementById("texto").innerHTML = e.results[0][0].transcript;
-  document.getElementById("texto2").innerHTML += e.results[e.results.length - 1][0].transcript;
+  document.getElementById("texto2").innerHTML = e.results[e.results.length - 1][0].transcript;
   //Con el + se añade a lo que ya estaba las nuevas palabras, sin el + se sustituye
   });
 
