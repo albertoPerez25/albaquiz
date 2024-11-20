e51b00fa757d204450a9a698131ceaf1ac6e077b
alberto <alberto.perez25@alu.uclm.es>
2024-11-20 22:25:48 +0100

    Menu idiomas

--- ajustes/ajustes.js
+++ ajustes/ajustes.js
@@ -3,4 +3,18 @@ import {animacionesSalidaNav,animacionesEntradaNav} from '../js/animaciones.js';
 window.onload = function() {
         animacionesEntradaNav();
         animacionesSalidaNav();
+        const btnIdioma = document.getElementById("idioma");
+        const cajaIdioma = document.getElementById("cajaIdioma");
+        const liIdioma = document.getElementsByClassName("liIdioma");
+        btnIdioma.addEventListener('click', () => {
+                cajaIdioma.classList.add('visible');
+        });
+        for(let i = 0; i < liIdioma.length; i++){
+                liIdioma[i].addEventListener('click', () => {
+                        setTimeout(function() { 
+                                cajaIdioma.classList.remove('visible');
+                                alert("Idioma cambiado a " + liIdioma[0].textContent);
+                        }, 300)
+                });
+        }
 }
