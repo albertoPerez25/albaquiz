142c57f992008e74aef6c14ab281d7915ae22a5e
alberto <alberto.perez25@alu.uclm.es>
2024-11-20 17:54:41 +0100

    ahora se ve la animacion del boton

--- js/index.js
+++ js/index.js
@@ -2,16 +2,17 @@ import {animacionesSalidaNav,animacionesEntradaNav} from './animaciones.js';
 
 
 window.onload = function() {
-        //Animacion al cargar la pagina
+//Animacion al cargar la pagina
         document.querySelector('body').style.opacity = 1;
         animacionesEntradaNav();
         animacionesSalidaNav();
         const btnJugar = document.getElementsByClassName("play-button")[0];
         btnJugar.addEventListener('click', () => {
-            document.querySelector('body').style.opacity = 0;
-            document.querySelector('body').style.background = '#fefff0';
-            setTimeout(function() { 
-                window.location.href = btnJugar.getAttribute('enlace');
-            }, 200)
+                setTimeout(function() { 
+                document.querySelector('body').style.opacity = 0;
+                document.querySelector('body').style.background = '#fefff0';
+                setTimeout(function() { 
+                        window.location.href = btnJugar.getAttribute('enlace');
+                }, 200)},200)
         });
 }
