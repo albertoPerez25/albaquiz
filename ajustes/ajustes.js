b9bcd64361ed258489efd94a177a88486bf3be09
vvalero <valentin.valero1@alu.uclm.es>
2024-11-21 08:39:42 +0100

    Conseguido que redirija a la página de inglés

--- ajustes/ajustes.js
+++ ajustes/ajustes.js
@@ -6,15 +6,22 @@ window.onload = function() {
         const btnIdioma = document.getElementById("idioma");
         const cajaIdioma = document.getElementById("cajaIdioma");
         const liIdioma = document.getElementsByClassName("liIdioma");
+
         btnIdioma.addEventListener('click', () => {
                 cajaIdioma.classList.add('visible');
         });
-        for(let i = 0; i < liIdioma.length; i++){
+
+        for (let i = 0; i < liIdioma.length; i++) {
                 liIdioma[i].addEventListener('click', () => {
                         setTimeout(function() { 
                                 cajaIdioma.classList.remove('visible');
+                                
+                                // Verificar si el idioma seleccionado es "Inglés"
+                                if (liIdioma[i].textContent.trim() === "Inglés") 
+                                        window.location.href = "../Inglés/ajustes/ajustesIngles.html";
                                 alert("Idioma cambiado a " + liIdioma[i].textContent);
-                        }, 300)
+
+                        }, 300);
                 });
         }
 }
