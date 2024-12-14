67eaf6b746ccf80b18f8182dfa3b18818cb8f0d9
alberto <alberto.perez25@alu.uclm.es>
2024-12-14 14:16:05 +0100

    aumentada ligeramente la variacion de la ubicacion

--- partida/partida.js
+++ partida/partida.js
@@ -48,7 +48,7 @@ window.onload = function () {
     let haUsadoAyuda = 0;
     let puntuacionCorrecta = 10;
     //Mapa
-    const variacionPermitida = 0.0001
+    const variacionPermitida = 0.0005
     var map = L.map('map');
 
     //Variables con los colores de css para animar el dado
